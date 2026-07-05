ab0319c17f1da57cfd47be5f27c26abe44ab8251
tomas-manriquez <tomas.manriquez@usach.cl>
2026-07-05 17:00:42 -0400

    refactor parse_args_test.go

--- backend/tests/parse_args_test.go
+++ backend/tests/parse_args_test.go
@@ -20,6 +20,63 @@ flag empty, args >1
 
 flag provided + args also provided
 */
+type testCase struct {
+	name string
+
+	path string
+	args []string
+
+	want string
+}
+
+func TestParseArgs (t *testing.T) {
+	tests := []testCase {
+		{
+			name: "path provided via flag",
+			path: "~/",
+			args: []string{},
+			want: "~/",
+		},
+		{
+			name: "path provided as positional argument",
+			path: "",
+			args: []string{"~/"},
+			want: "~/",
+		},
+		{
+			name: "path not provided neither in flag nor in positional arguments",
+			path: "",
+			args: []string{},
+			want: "",
+		},
+		{
+			name: "path provided as positional arguments with too many arguments",
+			path: "",
+			args: []string{"./","a"},
+			want: "",
+		},
+		{
+			name: "path provided both as flag and positional argument; flag is taken",
+			path: "~/",
+			args: []string{"./"},
+			want: "~/",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := utils.ParseArgs(tc.path, tc.args)
+
+			if got != tc.want {
+				t.Errorf(`ParseArgs(%q, %v) = %q, want %q`,
+					tc.path,
+					tc.args,
+					got,
+					tc.want,
+					)
+			}
+		})
+	}
+}
 
 /*
 Scenario 01
@@ -27,15 +84,6 @@ Given user compiled the backend
 When the user executes the program with the --path flag with a valid string
 Then the program assigns the value to var path and returns
 */
-func TestPathFromFlag(t *testing.T) {
-	path:= "~/"
-	args:= []string{"a", "b", "c"}
-	want:= "~/"
-	got := utils.ParseArgs(path, args)
-	if got != want {
-		t.Errorf(`ParseArgs("~/", []) = %q, want match for %#q`, got, want)
-	}
-}
 
 /*
 Scenario 02
@@ -43,15 +91,6 @@ Given user compiled the backend
 When the user executes the program with no --path flag but args has 1 valid string
 Then the program assigns the value to var path and returns
 */
-func TestPathFromArgs(t *testing.T) {
-	path:= ""
-	args:= []string{"~/"}
-	want:= "~/"
-	got := utils.ParseArgs(path, args)
-	if got != want {
-		t.Errorf(`ParseArgs("", ["~/"]) = %q, want match for %#q`, got, want)
-	}
-}
 
 /*
 Scenario 03
@@ -59,15 +98,6 @@ Given user compiled the backend
 When the user executes the program with no --path and no args
 Then the program keeps empty string to var path and returns
 */
-func TestNoPath(t *testing.T) {
-	path:= ""
-	args:= []string{}
-	want:= ""
-	got:= utils.ParseArgs(path, args)
-	if got != want {
-		t.Errorf(`ParseArgs("", []) = %q, want match for %#q`, got, want)
-	}
-}
 
 /*
 Scenario 04
@@ -75,15 +105,6 @@ Given user compiled the backend
 When the user executes the program with no --path but args has 2 or more values
 Then the program keeps empty string to var path and returns
 */
-func TestTooManyArgs(t *testing.T) {
-	path:= ""
-	args:= []string{"./", "a"}
-	want:= ""
-	got:= utils.ParseArgs(path, args)
-	if got != want {
-		t.Errorf(`ParseArgs("", ["./","a"]) = %q, want match for %#q`, got, want)
-	}
-}
 
 /*
 Scenario 05
