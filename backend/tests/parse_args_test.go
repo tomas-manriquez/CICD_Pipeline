1a4a855bc2a2a92070374909bfe08d3a4f49571d
tomas-manriquez <tomas.manriquez@usach.cl>
2026-07-05 16:18:32 -0400

    parse_args_test.go

--- backend/tests/parse_args_test.go
+++ backend/tests/parse_args_test.go
@@ -6,6 +6,20 @@ import (
 )
 
 //Feature: Read path to repo from input
+//I should write one test that loops every scenario??
+/*
+tests =
+
+flag provided
+
+flag empty, args contains one
+
+flag empty, args empty
+
+flag empty, args >1
+
+flag provided + args also provided
+*/
 
 /*
 Scenario 01
@@ -13,13 +27,13 @@ Given user compiled the backend
 When the user executes the program with the --path flag with a valid string
 Then the program assigns the value to var path and returns
 */
-func PathFromFlag(t *testing.T) {
+func TestPathFromFlag(t *testing.T) {
 	path:= "~/"
-	args:= ["a", "b", "c"]
-	want:= "Users/tomas"
-	path := ParseArgs(path, args)
-	if !want.MatchString(path) {
-		t.Errorf(`ParseArgs("~/") = %q, want match for %#q`, path, want)
+	args:= []string{"a", "b", "c"}
+	want:= "~/"
+	got := utils.ParseArgs(path, args)
+	if got != want {
+		t.Errorf(`ParseArgs("~/") = %q, want match for %#q`, got, want)
 	}
 }
 
@@ -29,3 +43,4 @@ Given user compiled the backend
 When the user executes the program with no --path flag but cargs has 1 valid string
 Then the program assigns the value to var path and returns
 */
+
