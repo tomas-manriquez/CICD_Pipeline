9b7f65ddbe66e73dcdb23224d0e86a5d07572861
tomas-manriquez <tomas.manriquez@usach.cl>
2026-07-05 16:43:55 -0400

    added tests in parse_args_test.go

--- backend/tests/parse_args_test.go
+++ backend/tests/parse_args_test.go
@@ -33,14 +33,70 @@ func TestPathFromFlag(t *testing.T) {
 	want:= "~/"
 	got := utils.ParseArgs(path, args)
 	if got != want {
-		t.Errorf(`ParseArgs("~/") = %q, want match for %#q`, got, want)
+		t.Errorf(`ParseArgs("~/", []) = %q, want match for %#q`, got, want)
 	}
 }
 
 /*
 Scenario 02
 Given user compiled the backend
-When the user executes the program with no --path flag but cargs has 1 valid string
+When the user executes the program with no --path flag but args has 1 valid string
 Then the program assigns the value to var path and returns
 */
+func TestPathFromArgs(t *testing.T) {
+	path:= ""
+	args:= []string{"~/"}
+	want:= "~/"
+	got := utils.ParseArgs(path, args)
+	if got != want {
+		t.Errorf(`ParseArgs("", ["~/"]) = %q, want match for %#q`, got, want)
+	}
+}
 
+/*
+Scenario 03
+Given user compiled the backend
+When the user executes the program with no --path and no args
+Then the program keeps empty string to var path and returns
+*/
+func TestNoPath(t *testing.T) {
+	path:= ""
+	args:= []string{}
+	want:= ""
+	got:= utils.ParseArgs(path, args)
+	if got != want {
+		t.Errorf(`ParseArgs("", []) = %q, want match for %#q`, got, want)
+	}
+}
+
+/*
+Scenario 04
+Given user compiled the backend
+When the user executes the program with no --path but args has 2 or more values
+Then the program keeps empty string to var path and returns
+*/
+func TestTooManyArgs(t *testing.T) {
+	path:= ""
+	args:= []string{"./", "a"}
+	want:= ""
+	got:= utils.ParseArgs(path, args)
+	if got != want {
+		t.Errorf(`ParseArgs("", ["./","a"]) = %q, want match for %#q`, got, want)
+	}
+}
+
+/*
+Scenario 05
+Given user compiled the backend
+When the user executes the program with a --path flag value and args has values
+Then the program assigns to path the value in the --path flag
+*/
+func TestFlagBeforeArgs(t *testing.T) {
+	path:= "~/"
+	args:= []string{"./"}
+	want:= "~/"
+	got:= utils.ParseArgs(path, args)
+	if got != want {
+		t.Errorf(`ParseArgs("~/", ["./"]) = %q, want match for %#q`, got, want)
+	}
+}
